113e85295619562d71dff25224f368b8ffcfe1c3
Mooje Kong <mjkong@google.com>
2025-09-17 21:55:00 +0900

    modify remote state for project

--- terraform/02.configEnv/data.tf
+++ terraform/02.configEnv/data.tf
@@ -1,7 +1,15 @@
-data "terraform_remote_state" "onboard-demo-project" {
-  backend = "local"
+# data "terraform_remote_state" "onboard-demo-project" {
+#   backend = "local"
 
+#   config = {
+#     path = "../01.project/terraform.tfstate"
+#   }
+# }
+
+data "terraform_remote_state" "onboard-demo-project" {
+  backend = "gcs"
   config = {
-    path = "../01.project/terraform.tfstate"
+    bucket = "onboard-nonprod-remote-state-bucket"
+    prefix = "project"
   }
 }
