8b7c9078023d96b515ec24613a566641c70d41e7
Mooje Kong <mjkong@google.com>
2025-09-22 01:27:33 +0900

    typo

--- terraform/02.configEnv/locals.tf
+++ terraform/02.configEnv/locals.tf
@@ -1,7 +1,7 @@
 locals {
   project_id = data.terraform_remote_state.onboard-demo-project.outputs.project_id
-  sdp_inspect_template = "projects/${local.project_id}/locations/${var.sdp_region}/inspectTemplates/${sdp_inspect_name}"
-  sdp_deidentify_template = "projects/${local.project_id}/locations/${var.sdp_region}/deidentifyTemplates/${sdp_deidentify_name}"
+  sdp_inspect_template = "projects/${local.project_id}/locations/${var.sdp_region}/inspectTemplates/${var.sdp_inspect_name}"
+  sdp_deidentify_template = "projects/${local.project_id}/locations/${var.sdp_region}/deidentifyTemplates/${var.sdp_deidentify_name}"
 
   project_iam_roles = [
     "roles/storage.objectUser",
